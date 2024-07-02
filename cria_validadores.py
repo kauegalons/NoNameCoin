3a6c09a31e0d982531aabcaaed2e85cbe71fe934
Kaue Galon <kauegalons@gmail.com>
2024-07-02 14:55:34 -0300

    Add mais validador

--- cria_validadores.py
+++ cria_validadores.py
@@ -2,12 +2,13 @@ import requests
 
 # Registrando Validadores
 
-response = requests.post('http://localhost:5001/seletor/register/b/69.0')
+response = requests.post('http://localhost:5001/seletor/register/a/10.0')
 print(response.text)
-response = requests.post('http://localhost:5001/seletor/register/c/666.0')
+response = requests.post('http://localhost:5001/seletor/register/b/11.0')
 print(response.text)
-response = requests.post('http://localhost:5001/seletor/register/d/6969.0')
+response = requests.post('http://localhost:5001/seletor/register/c/12.0')
 print(response.text)
-response = requests.post('http://localhost:5001/seletor/register/e/9669.0')
+response = requests.post('http://localhost:5001/seletor/register/d/13.0')
 print(response.text)
-
+response = requests.post('http://localhost:5001/seletor/register/e/69.0')
+print(response.text)
